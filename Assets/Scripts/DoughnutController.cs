16b2c09c3c46750ea795218bd076e164e2919e47
Gregory Rose <greg@ncroses.com>
2014-11-20 18:50:34 -0500

    hovering donuts

---
+++ Assets/Scripts/DoughnutController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoughnutController : MonoBehaviour {
+
+	private Vector2 startPos;
+
+	// Use this for initialization
+	void Start () {
+		startPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float offset = Mathf.Sin(Time.time*2) * collider2D.bounds.size.y * 0.25f;
+		transform.position = startPos + new Vector2(0, offset);
+	}
+}
